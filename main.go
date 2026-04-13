25d1b53697ef4a0b504653580f002d04b2144a30
muratkadirbay-droid <muratkadirbay@gmail.com>
2026-04-13 12:09:47 +0500

    added my phrases

--- main.go
+++ main.go
@@ -3,5 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+    fmt.Println("Я сегодня изучил Go")
+    fmt.Println("Моя любимая песня: Believer")
+    fmt.Println("Исполнитель: Imagine Dragons")
 }
